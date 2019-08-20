75f502d4aa84743e3f7c57aa7beec93195c830f3
gkasieczka <g.kasieczka@gmail.com>
2019-08-20 10:31:39 +0200

    Update Disco.py

--- Disco.py
+++ Disco.py
@@ -1,25 +1,26 @@
-def distance_corr(yqcd,mqcd,normedweight,power=1):
-    xx = yqcd.view(-1, 1).repeat(1, len(yqcd)).view(len(yqcd),len(yqcd))
-    yy = yqcd.repeat(len(yqcd),1).view(len(yqcd),len(yqcd))
-    amat=(xx-yy).abs()
+def distance_corr(var_1,var_2,normedweight,power=1):
+    
+    xx = var_1.view(-1, 1).repeat(1, len(var_1)).view(len(var_1),len(var_1))
+    yy = var_1.repeat(len(var_1),1).view(len(var_1),len(var_1))
+    amat = (xx-yy).abs()
 
-    xx = mqcd.view(-1, 1).repeat(1, len(mqcd)).view(len(mqcd),len(mqcd))
-    yy = mqcd.repeat(len(mqcd),1).view(len(mqcd),len(mqcd))
-    bmat=(xx-yy).abs()
+    xx = var_2.view(-1, 1).repeat(1, len(var_2)).view(len(var_2),len(var_2))
+    yy = var_2.repeat(len(var_2),1).view(len(var_2),len(var_2))
+    bmat = (xx-yy).abs()
 
-    amatavg=torch.mean(amat*normedweight,dim=1)
-    Amat=amat-amatavg.repeat(len(yqcd),1).view(len(yqcd),len(yqcd))\
-        -amatavg.view(-1, 1).repeat(1, len(yqcd)).view(len(yqcd),len(yqcd))\
+    amatavg = torch.mean(amat*normedweight,dim=1)
+    Amat=amat-amatavg.repeat(len(var_1),1).view(len(var_1),len(var_1))\
+        -amatavg.view(-1, 1).repeat(1, len(var_1)).view(len(var_1),len(var_1))\
         +torch.mean(amatavg*normedweight)
 
-    bmatavg=torch.mean(bmat*normedweight,dim=1)
-    Bmat=bmat-bmatavg.repeat(len(mqcd),1).view(len(mqcd),len(mqcd))\
-        -bmatavg.view(-1, 1).repeat(1, len(mqcd)).view(len(mqcd),len(mqcd))\
+    bmatavg = torch.mean(bmat*normedweight,dim=1)
+    Bmat=bmat-bmatavg.repeat(len(var_2),1).view(len(var_2),len(var_2))\
+        -bmatavg.view(-1, 1).repeat(1, len(var_2)).view(len(var_2),len(var_2))\
         +torch.mean(bmatavg*normedweight)
 
-    ABavg=torch.mean(Amat*Bmat*normedweight,dim=1)
-    AAavg=torch.mean(Amat*Amat*normedweight,dim=1)
-    BBavg=torch.mean(Bmat*Bmat*normedweight,dim=1)
+    ABavg = torch.mean(Amat*Bmat*normedweight,dim=1)
+    AAavg = torch.mean(Amat*Amat*normedweight,dim=1)
+    BBavg = torch.mean(Bmat*Bmat*normedweight,dim=1)
 
     if(power==1):
         dCorr=(torch.mean(ABavg*normedweight))/torch.sqrt((torch.mean(AAavg*normedweight)*torch.mean(BBavg*normedweight)))
@@ -27,6 +28,7 @@ def distance_corr(yqcd,mqcd,normedweight,power=1):
         dCorr=(torch.mean(ABavg*normedweight))**2/(torch.mean(AAavg*normedweight)*torch.mean(BBavg*normedweight))
     else:
         dCorr=(torch.mean(ABavg*normedweight))/torch.sqrt((torch.mean(AAavg*normedweight)*torch.mean(BBavg*normedweight)))**power
+    
     return dCorr
 
 
